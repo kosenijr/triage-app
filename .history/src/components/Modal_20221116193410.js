4e8c077820095ff2a251b0e784a47d477c6682b9
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-11-18 12:51:20 -0500

    made updates to modal

---
+++ .history/src/components/Modal_20221116193410.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import styles from '../styles/Modal.module.css'
+import { GrClose } from "react-icons/gr";
+
+
+
+export default function Modal({ onCreatePatient }) {
+    const [name, setName] = useState('');
+    const [dob, setDOB] = useState('');
+    const [complaint, setComplaint] = useState('');
+    const [priority, setPriority] = useState(3);
+    const [room, setRoom] = useState('triage');
+    const [stage, setStage] = useState('triage');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const patient = {
+            name,
+            dob,
+            complaint,
+            priority,
+            room,
+            stage
+        }
+        onCreatePatient(patient);
+    }
+
+    return (
+        <div className={styles['modal']}>
+            < GrClose /
+            <form action="">
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" value={dob} onChange={(e) => setDOB(e.target.value)} />
+                <input type="text" value={complaint} onChange={(e) => setComplaint(e.target.value)} />
+                <input type="text" value={priority} onChange={(e) => setPriority(e.target.value)} />
+                <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} />
+                <input type="text" value={stage} onChange={(e) => setStage(e.target.value)} />
+                <button onClick={(e) => onSubmit(e)}>Submit</button>
+            </form>
+        </div>
+
+    )
+}
---
+++ .history/src/components/Modal_20221116195043.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import styles from '../styles/Modal.module.css'
+import { GrClose } from "react-icons/gr";
+
+
+
+export default function Modal({ onCreatePatient, setIsModalOpen }) {
+    const [name, setName] = useState('');
+    const [dob, setDOB] = useState('');
+    const [complaint, setComplaint] = useState('');
+    const [priority, setPriority] = useState(3);
+    const [room, setRoom] = useState('triage');
+    const [stage, setStage] = useState('triage');
+
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const patient = {
+            name,
+            dob,
+            complaint,
+            priority,
+            room,
+            stage
+        }
+        onCreatePatient(patient);
+    }
+
+    return (
+        <div className={styles['modal']}>
+            < GrClose className={styles['cancel-modal']} onClick={() => setIsModalOpen(false)} />
+            <form action="">
+                <div>
+                    <label htmlFor="name"></label>
+                    <input type="text" value={name} id="name" onChange={(e) => setName(e.target.value)} />
+                </div>
+                <input type="text" value={dob} onChange={(e) => setDOB(e.target.value)} />
+                <input type="text" value={complaint} onChange={(e) => setComplaint(e.target.value)} />
+                <input type="text" value={priority} onChange={(e) => setPriority(e.target.value)} />
+                <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} />
+                <input type="text" value={stage} onChange={(e) => setStage(e.target.value)} />
+                <button onClick={(e) => onSubmit(e)}>Submit</button>
+            </form>
+        </div>
+
+    )
+}
