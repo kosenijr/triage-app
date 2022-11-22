7a51cee9771b4a22f1e63eaa7bd2589aafffc049
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2022-11-21 19:54:03 -0500

    made changes to modal button and patient card buttons

---
+++ .history/src/components/PatientCard_20221121191516.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import styles from '../styles/PatientCard.module.css'
+import { ROUTES } from '../constants'
+import ReactReadMoreReadLess from 'react-read-more-read-less'
+
+const PatientCard = ({ patient, onStartDischarge, onCompleteDischarge, updateRoomAssignment }) => {
+    const [priority, setPriority] = useState(patient.priority);
+    const onPriorityChange = (newPriority) => {
+        setPriority(newPriority);
+        const newPatientRecord = {
+            ...patient,
+            priority: newPriority
+        }
+        const requestOptions = {
+            method: 'PUT',
+            body: JSON.stringify(newPatientRecord),
+        };
+
+        fetch(ROUTES['GET_EACH_PATIENT'], requestOptions)
+            .then(result => result.json())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+        
+    }
+    return (
+        <div className={styles['patient-card']}>
+            <div className={styles['inner-patient-card']}>
+                <h3>{patient?.name}</h3>
+                <div className={styles['card-priority']}>
+                    <h4>Priority</h4>
+                    <span className={priority === 1 ? 'font-bold' : ''} onClick={() => onPriorityChange(1)}>1</span>
+                    <span className={priority === 2 ? 'font-bold' : ''} onClick={() => onPriorityChange(2)}>2</span>
+                    <span className={priority === 3 ? 'font-bold' : ''} onClick={() => onPriorityChange(3)}>3</span>
+                </div>
+                <p>Date of Birth: {patient?.dob}</p>
+                <h4>Chief Complaint:</h4>
+
+                <ReactReadMoreReadLess charLimit={10} >
+                    {patient?.complaint}
+                
+                </ReactReadMoreReadLess>
+                
+                {patient?.stage === 'triage' &&
+                    <div>
+                        <label htmlFor="assign-room">Assign to room:</label>
+                        <select name="rooms" id="assign-room" onChange={(e) => updateRoomAssignment(patient, e.target.value)}>
+                            <option value="">--Please choose an option--</option>
+                            <option value="treating-room">Treating Room</option>
+                            <option value="discharging-room">Discharging Room</option>
+
+                        </select>
+                    </div>
+                }
+                {patient?.stage === 'treating' && <button className={styles["start-button"]} onClick={() => onStartDischarge(patient)}>Start Discharge Process</button>}
+
+                {patient?.stage === 'discharging' && <button className={styles["complete-button"]} onClick={() => onCompleteDischarge(patient)}>Discharge Process Complete</button>}
+
+
+            </div >
+        </div>
+
+    )
+}
+
+export default PatientCard
